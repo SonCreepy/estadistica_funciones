8118d81591304ea84c89d5c0c1930ae801f54a33
Marco <marcotoscanomarcos@gmail.com>
2026-09-25 20:20:54 +0200

    Added Basic Functions
    Will update it to adapt the table manually and eliminate the need of using N

--- Unit2Functions.R
+++ Unit2Functions.R
@@ -1,3 +1,27 @@
+#Copyright Marco Toscano 2026©. Do not distribute
+
+# DISCLAIMER: If using relative frequencies, N=1 !!
+
+Center_Gravity <- function(row, col, table, N) {
+  return(c(x_meanF(row, table, N), y_meanF(col, table, N)))
+}
+
+
+
+x_meanF <- function(row, table, N) {
+  return(sum(row*rowSums(table))/N)
+}
+
+
+
+
+y_meanF <- function(col, table, N) {
+  return(sum(col*colSums(table))/N)
+}
+
+
+
+
 mom_ord2 <- function(row, col, table, r, s, a,b) {
   N <- sum(table)
   sum <- 0
@@ -19,3 +43,21 @@ mom_ord2 <- function(row, col, table, r, s, a,b) {
 #a -> reference point of x factor
 #b -> reference point of y factor
 
+#For ordinary moments, a,b=0,0, for central moments, a,b=x_mean,y_mean
+
+
+Covariance <- function(row, col, table, N) {
+  m11 <- mom_ord2(row, col, table, 1, 1, 0, 0)
+  
+  return(m11 - x_meanF(row, table, N)*y_meanF(col, table, N))
+}
+
+
+SecondOrd_momentX <- function(row, col, table, N) {
+  return(mom_ord2(row,col,table,2,0,0,0) - (x_meanF(row,table,N)^2))
+}  
+
+
+SecondOrd_momentY <- function(row, col, table, N) {
+  return(mom_ord2(row,col,table,0,2,0,0) - (y_meanF(col,table,N)^2))
+}
