98fd7f1487f626c8f3a38d4b9c34271f3dc0a467
Sergio Torres <sergiotormorales@gmail.com>
2026-09-26 16:39:15 +0200

    Add files via upload

--- areCorrelated().R
+++ areCorrelated().R
@@ -1,3 +1,3 @@
 areCorrelated <- function(data_x,data_y,freqs,N){
-  return(areIndependent(data_x,data_y,freqs) || Covariance(data_x,data_y,table,N))
+  return(areIndependent(data_x,data_y,freqs) || Covariance(data_x,data_y,table,N) != 0)
 }
